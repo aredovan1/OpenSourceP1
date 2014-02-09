5195b94f6405ccd1a762939a84b4eaa92c4e03dd
Jinno <aredovan1@luc.edu>
2014-02-09 17:42:51 -0600

    first up

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by jinno on 2/9/14.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
